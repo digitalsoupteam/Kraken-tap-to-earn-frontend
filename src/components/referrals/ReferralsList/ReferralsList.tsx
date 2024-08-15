f99382e7f4701f213bdf2ddf36a5225c356b898c
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2024-08-15 18:29:27 +0400

    Referrals list

--- src/components/referrals/ReferralsList/ReferralsList.tsx
+++ src/components/referrals/ReferralsList/ReferralsList.tsx
@@ -1,17 +1,50 @@
 import React, {FC} from 'react';
 
 import styles from './ReferralsList.module.css';
+import {UserItem} from "@/components/common";
 
 const ReferralsList: FC = () => {
+    const referrals = [
+        {
+            id: 'user1',
+            name: 'J. Smith',
+            points: 100000000
+        },
+        {
+            id: 'user2',
+            name: 'J. Smith',
+            points: 100000000
+        },
+        {
+            id: 'user3',
+            name: 'J. Smith',
+            points: 100000000
+        },
+        {
+            id: 'user4',
+            name: 'J. Smith',
+            points: 100000000
+        },
+        {
+            id: 'user5',
+            name: 'J. Smith',
+            points: 100000000
+        },
+    ];
+
     return <div className={styles.root}>
         <div className={styles.title}>
             Your referrals
         </div>
 
-        {/*<div className={styles.list}>*/}
-        {/*</div>*/}
+        {
+            referrals ? <div className={styles.list}>
+                {referrals && referrals.length && referrals.map(referral => (
+                    <UserItem name={referral.name} points={referral.points} key={referral.id}/>
+                ))}
+            </div> : <div className={styles.noReferrals}>You haven&apos;t invited anyone yet</div>
+        }
 
-        <div className={styles.noReferrals}>You haven&apos;t invited anyone yet</div>
     </div>
 };
 
