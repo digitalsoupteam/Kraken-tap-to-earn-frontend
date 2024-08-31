82d1fd749c29d4918d2421ca08e529511af248ed
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2024-08-31 23:26:31 +0400

    Quests upd

--- src/components/quests/QuestList/QuestList.tsx
+++ src/components/quests/QuestList/QuestList.tsx
@@ -1,52 +1,74 @@
-import React, {FC} from 'react';
+import React, { FC } from 'react';
+
+import { QuestItem } from "@/components/quests";
 
 import styles from './QuestList.module.css';
-import {QuestItem} from "@/components/quests";
+
+interface IQuestItem {
+    id: number;
+    name: string;
+    points: number;
+    objective: string;
+    link: string;
+    status: 'completed' | 'failed' | 'not-started' | 'verifying';
+}
 
 const QuestList: FC = () => {
-    const questList = [
-        {
-            id: 'quest0',
-            name: 'Subscribe to our youtube',
-            points: 20000,
-            objective: 'youtube',
-            link: '#',
-            status: 'not-started',
-        },
-        {
-            id: 'quest1',
-            name: 'Subscribe to our youtube',
-            points: 20000,
-            objective: 'telegram',
-            link: '#',
-            status: 'not-started',
-        },
-        {
-            id: 'quest2',
-            name: 'Subscribe to our youtube',
-            points: 20000,
-            objective: 'youtube',
-            link: '#',
-            status: 'complete',
-        },
-        {
-            id: 'quest3',
-            name: 'Subscribe to our youtube',
-            points: 20000,
-            objective: 'telegram',
-            link: '#',
-            status: 'complete',
-        },
-    ];
+    // const questList = [
+    //     {
+    //         id: 'quest0',
+    //         name: 'Subscribe to our youtube',
+    //         points: 20000,
+    //         objective: 'youtube',
+    //         link: '#',
+    //         status: 'not-started',
+    //     },
+    //     {
+    //         id: 'quest1',
+    //         name: 'Subscribe to our youtube',
+    //         points: 20000,
+    //         objective: 'telegram',
+    //         link: '#',
+    //         status: 'not-started',
+    //     },
+    //     {
+    //         id: 'quest2',
+    //         name: 'Subscribe to our youtube',
+    //         points: 20000,
+    //         objective: 'youtube',
+    //         link: '#',
+    //         status: 'completed',
+    //     },
+    //     {
+    //         id: 'quest3',
+    //         name: 'Subscribe to our youtube',
+    //         points: 20000,
+    //         objective: 'telegram',
+    //         link: '#',
+    //         status: 'completed',
+    //     },
+    // ];
 
-    return <div className={styles.root}>
-        {questList && questList.length && questList.map(quest => (
-            <QuestItem name={quest.name} points={quest.points} href={quest.link} objective={quest.objective} status={quest.status}
-                       key={quest.id}/>
-        ))}
+    const questList: IQuestItem[] = [];
 
-        {!questList && <div className={styles.noQuests}>There are currently no quests available</div>}
-    </div>
+    return (
+        <div className={styles.root}>
+            {questList.length > 0 ? (
+                questList.map(quest => (
+                    <QuestItem
+                        name={quest.name}
+                        points={quest.points}
+                        href={quest.link}
+                        objective={quest.objective}
+                        status={quest.status}
+                        key={quest.id}
+                    />
+                ))
+            ) : (
+                <div className={styles.noQuests}>There are currently no quests available</div>
+            )}
+        </div>
+    );
 };
 
 export default QuestList;
