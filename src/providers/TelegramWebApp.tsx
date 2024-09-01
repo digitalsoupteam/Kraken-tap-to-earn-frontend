f59d7197fe5b37c5834b72d3945a3e213d3a03c2
KirillBashorin <fumhejkeee@gmail.com>
2024-09-02 00:21:57 +0400

    Haptic test v2

--- src/providers/TelegramWebApp.tsx
+++ src/providers/TelegramWebApp.tsx
@@ -29,52 +29,56 @@ const TelegramWebApp: FC<PropsWithChildren> = ({children}) => {
     }, []);
 
     return <>
-        <div style={{zIndex: 999, position: 'absolute'}}>
-            <button onClick={() => {
-                WebApp.HapticFeedback.impactOccurred('light')
-            }} style={{padding: 10}}>impactOccurred - light
-            </button>
+        {
+            typeof window !== 'undefined' && <>
+                <div style={{zIndex: 999, position: 'absolute'}}>
+                    <button onClick={() => {
+                        WebApp.HapticFeedback.impactOccurred('light')
+                    }} style={{padding: 10}}>impactOccurred - light
+                    </button>
 
-            <button onClick={() => {
-                WebApp.HapticFeedback.impactOccurred('medium')
-            }} style={{padding: 10}}>impactOccurred - medium
-            </button>
+                    <button onClick={() => {
+                        WebApp.HapticFeedback.impactOccurred('medium')
+                    }} style={{padding: 10}}>impactOccurred - medium
+                    </button>
 
-            <button onClick={() => {
-                WebApp.HapticFeedback.impactOccurred('heavy')
-            }} style={{padding: 10}}>impactOccurred - heavy
-            </button>
+                    <button onClick={() => {
+                        WebApp.HapticFeedback.impactOccurred('heavy')
+                    }} style={{padding: 10}}>impactOccurred - heavy
+                    </button>
 
-            <button onClick={() => {
-                WebApp.HapticFeedback.impactOccurred('rigid')
-            }} style={{padding: 10}}>impactOccurred - rigid
-            </button>
+                    <button onClick={() => {
+                        WebApp.HapticFeedback.impactOccurred('rigid')
+                    }} style={{padding: 10}}>impactOccurred - rigid
+                    </button>
 
-            <button onClick={() => {
-                WebApp.HapticFeedback.impactOccurred('soft')
-            }} style={{padding: 10}}>impactOccurred - soft
-            </button>
+                    <button onClick={() => {
+                        WebApp.HapticFeedback.impactOccurred('soft')
+                    }} style={{padding: 10}}>impactOccurred - soft
+                    </button>
 
-            <button onClick={() => {
-                WebApp.HapticFeedback.notificationOccurred('error')
-            }} style={{padding: 10}}>notificationOccurred - error
-            </button>
+                    <button onClick={() => {
+                        WebApp.HapticFeedback.notificationOccurred('error')
+                    }} style={{padding: 10}}>notificationOccurred - error
+                    </button>
 
-            <button onClick={() => {
-                WebApp.HapticFeedback.notificationOccurred('success')
-            }} style={{padding: 10}}>notificationOccurred - success
-            </button>
+                    <button onClick={() => {
+                        WebApp.HapticFeedback.notificationOccurred('success')
+                    }} style={{padding: 10}}>notificationOccurred - success
+                    </button>
 
-            <button onClick={() => {
-                WebApp.HapticFeedback.notificationOccurred('warning')
-            }} style={{padding: 10}}>notificationOccurred - warning
-            </button>
+                    <button onClick={() => {
+                        WebApp.HapticFeedback.notificationOccurred('warning')
+                    }} style={{padding: 10}}>notificationOccurred - warning
+                    </button>
 
-            <button onClick={() => {
-                WebApp.HapticFeedback.selectionChanged()
-            }} style={{padding: 10}}>notificationOccurred - warning
-            </button>
-        </div>
+                    <button onClick={() => {
+                        WebApp.HapticFeedback.selectionChanged()
+                    }} style={{padding: 10}}>notificationOccurred - warning
+                    </button>
+                </div>
+            </>
+        }
         {children}
     </>
 };
