d209d6f92459cddc08720036cdcc22e2fbad9d2a
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2024-10-08 17:49:52 +0400

    Energy tracker fix

--- src/components/game/EnergyTracker/EnergyTracker.tsx
+++ src/components/game/EnergyTracker/EnergyTracker.tsx
@@ -133,7 +133,7 @@ const EnergyTracker: FC = () => {
 
             if (response.id !== 1000) return;
 
-            setCalmUntil(response.result.calm_until);
+            setCalmUntil(response.result.calmUntil);
         }, [lastMessage]);
 
         return <div className={styles.root}>
