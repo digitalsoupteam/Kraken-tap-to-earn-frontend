4abf40fdfcc62d3bc19b00fa0d737d098dfed22e
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2024-10-12 20:42:12 +0400

    TapEffect canvas

--- src/components/game/TapButton/TapButton.tsx
+++ src/components/game/TapButton/TapButton.tsx
@@ -1,39 +1,38 @@
 'use client';
 
-import React, {FC, useState, useRef, useEffect} from 'react';
+import React, { FC, useState, useRef, useEffect } from 'react';
 import WebApp from "@twa-dev/sdk";
-import {motion} from 'framer-motion';
 import useSound from 'use-sound';
-
 import KrakenBottomImage from '/public/images/kraken.svg';
 import KrakenSeasideImage from '/public/images/kraken-smirking.svg';
 import KrakenTroposphereImage from '/public/images/kraken-smiling.svg';
 import KrakenOuterSpaceImage from '/public/images/kraken-rock.svg';
-import {useGameStore} from '@/components/game';
+import { useGameStore } from '@/components/game';
 import useWebSocketStore from "@/stores/useWebSocketStore";
-
 import styles from './TapButton.module.css';
 
 const krakens = [
-    <KrakenBottomImage key={'first-level'}/>,
-    <KrakenSeasideImage key={'second-level'}/>,
-    <KrakenTroposphereImage key={'third-level'}/>,
-    <KrakenOuterSpaceImage key={'fourth-level'}/>,
+    <KrakenBottomImage key={'first-level'} />,
+    <KrakenSeasideImage key={'second-level'} />,
+    <KrakenTroposphereImage key={'third-level'} />,
+    <KrakenOuterSpaceImage key={'fourth-level'} />,
 ];
 
 const TapButton: FC = () => {
     const [playSound] = useSound('/sounds/tap.wav');
     const tapEffectDuration = 500;
-    const [taps, setTaps] = useState<{ id: number, x: number, y: number }[]>([]);
+    const [taps, setTaps] = useState<{ id: number, x: number, y: number, startTime: number }[]>([]);
     const isTouchDevice = typeof window !== 'undefined' && window.matchMedia('(pointer: coarse)').matches;
     const buttonRef = useRef<HTMLButtonElement>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
     const [isDisabled, setIsDisabled] = useState(true);
+    const [tapsCounter, setTapsCounter] = useState(0);
 
     const {
         multiplier,
         setTotalPoints,
         sessionLeft,
-        setSessionLeft, sessionUntil,
+        setSessionLeft,
         setSessionUntil,
         setSessionStart,
         setCalmUntil,
@@ -44,7 +43,6 @@ const TapButton: FC = () => {
         setTotalPoints: state.setTotalPoints,
         sessionLeft: state.sessionLeft,
         setSessionLeft: state.setSessionLeft,
-        sessionUntil: state.sessionUntil,
         setSessionUntil: state.setSessionUntil,
         setSessionStart: state.setSessionStart,
         setCalmUntil: state.setCalmUntil,
@@ -60,10 +58,35 @@ const TapButton: FC = () => {
         lastMessage: state.lastMessage
     }));
 
+    const drawTapEffects = () => {
+        const canvas = canvasRef.current;
+        if (!canvas) return;
+
+        const ctx = canvas.getContext('2d');
+        if (!ctx) return;
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        const currentTime = performance.now();
+
+        taps.forEach(tap => {
+            const elapsedTime = (currentTime - tap.startTime) / 1000;
+            const opacity = Math.max(1 - elapsedTime / (tapEffectDuration / 1000), 0);
+            const yOffset = -20 - (80 * elapsedTime);
+
+            const rootStyles = getComputedStyle(document.body);
+            const fontFamily = rootStyles.getPropertyValue('--dark-hornet-italic').trim();
+            ctx.font = `20px ${fontFamily}`;
+            ctx.fillStyle = `rgba(255, 255, 255, ${opacity})`;
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            ctx.fillText(`+${multiplier}`, tap.x, tap.y + yOffset);
+        });
+
+        setTaps(prevTaps => prevTaps.filter(tap => (performance.now() - tap.startTime) < tapEffectDuration));
+    };
+
     const handleTap = (clientX: number, clientY: number, touchIdentifier?: number) => {
         playSound();
-
-        const id = Date.now() + (touchIdentifier || 0);
 
         const buttonRect = buttonRef.current?.getBoundingClientRect();
         if (!buttonRect) return;
@@ -81,24 +104,17 @@ const TapButton: FC = () => {
             }],
         };
 
-        console.log(`[LOG]: Call sendTaps method, with data`, message);
         sendMessage(JSON.stringify(message));
-
-        if (typeof window !== 'undefined') {
-            isVibrationOn && WebApp.HapticFeedback.impactOccurred('heavy');
+        if (isVibrationOn && typeof window !== 'undefined') {
+            WebApp.HapticFeedback.impactOccurred('heavy');
         }
 
-        setTaps(prev => [...prev, {id, x, y}]);
-
-        setTimeout(() => {
-            setTaps((prev) => prev.filter(tap => tap.id !== id));
-        }, tapEffectDuration);
+        setTapsCounter(tapsCounter + 1);
+        setTaps(prevTaps => [...prevTaps, { id: Date.now(), x, y, startTime: performance.now() }]);
     };
 
     const handleTouch = (e: React.TouchEvent<HTMLButtonElement>) => {
-        if (isDisabled) return;
-
-        if (!isTouchDevice) return;
+        if (isDisabled || !isTouchDevice) return;
 
         Array.from(e.touches).forEach(touch => {
             handleTap(touch.clientX, touch.clientY, touch.identifier);
@@ -106,9 +122,7 @@ const TapButton: FC = () => {
     };
 
     const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-        if (isDisabled) return;
-
-        if (isTouchDevice) return;
+        if (isDisabled || isTouchDevice) return;
 
         handleTap(e.clientX, e.clientY);
     };
@@ -117,23 +131,12 @@ const TapButton: FC = () => {
         if (!lastMessage) return;
         const response = JSON.parse(lastMessage);
 
-        if (response.id != 2000) return
-
-        console.log(`[LOG]: Receive sendTaps data`, response);
-        if (
-            !response ||
-            !response.result ||
-            !response.result.userInfo
-        )
-            return;
-
+        if (response.id !== 2000) return;
         const userInfoFromTap = response.result.userInfo;
-        console.log(`[LOG]: Parse user from sendTaps data`, userInfoFromTap);
-
         setTotalPoints(parseFloat(userInfoFromTap.points.toFixed(1)));
         setSessionLeft(userInfoFromTap.sessionLeft);
-        setSessionStart(userInfoFromTap.sessionStart);
         setSessionUntil(userInfoFromTap.sessionUntil);
+        setSessionStart(userInfoFromTap.sessionStart);
         setCalmUntil(userInfoFromTap.calmUntil);
     }, [lastMessage]);
 
@@ -141,31 +144,43 @@ const TapButton: FC = () => {
         setIsDisabled(!sessionLeft);
     }, [sessionLeft]);
 
-    return <div className={styles.root}>
-        <button className={styles.button} ref={buttonRef} onTouchStart={handleTouch} onClick={handleClick}
-                disabled={isDisabled}>
-            <motion.span className={styles.buttonInner}
-                         whileTap={isDisabled ? {} : {scale: 0.9, transition: {duration: 0.3}}}>
-                <span className={styles.image}>
-                    <span className={styles.buttonBg}/>
-                    {krakens[level]}
-                </span>
-            </motion.span>
-        </button>
+    useEffect(() => {
+        const animationFrame = requestAnimationFrame(drawTapEffects);
+        return () => cancelAnimationFrame(animationFrame);
+    }, [taps]);
 
-        {taps.map((tap) => (
-            <motion.div
-                key={tap.id}
-                className={styles.tapEffect}
-                initial={{opacity: 1, y: -20}}
-                animate={{opacity: 0, y: -100}}
-                transition={{duration: tapEffectDuration / 1000}}
-                style={{top: tap.y, left: tap.x}}
-            >
-                +{multiplier}
-            </motion.div>
-        ))}
-    </div>
+    return (
+        <div className={styles.root}>
+            <div
+                style={{
+                    position: 'fixed',
+                    top: '50px',
+                    left: '50px',
+                }}
+            >{tapsCounter}</div>
+
+            <button
+                className={styles.button}
+                ref={buttonRef}
+                onTouchEnd={handleTouch}
+                onClick={handleClick}
+                disabled={isDisabled}>
+                <span className={styles.buttonInner}>
+                    <span className={styles.image}>
+                        <span className={styles.buttonBg} />
+                        {krakens[level]}
+                    </span>
+                </span>
+            </button>
+
+            <canvas
+                ref={canvasRef}
+                className={styles.canvas}
+                width={buttonRef.current?.offsetWidth}
+                height={buttonRef.current?.offsetHeight}
+            />
+        </div>
+    );
 };
 
 export default TapButton;
