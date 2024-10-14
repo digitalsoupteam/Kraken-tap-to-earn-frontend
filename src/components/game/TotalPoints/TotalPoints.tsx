f22d90b1120cce77bccfd1fb2845edd0f36ed772
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2024-10-14 18:02:53 +0400

    Total points transition

--- src/components/game/TotalPoints/TotalPoints.tsx
+++ src/components/game/TotalPoints/TotalPoints.tsx
@@ -1,26 +1,48 @@
 'use client';
-import React, {FC} from 'react'
-
+import React, { FC, useEffect, useState } from 'react';
+import { motion, useMotionValue, useSpring } from 'framer-motion';
 import DiamondPointsIcon from '/public/images/diamond-points.svg';
 import CrossIcon from '/public/images/cross-mini.svg';
-import {useGameStore} from "@/components/game";
+import { useGameStore } from "@/components/game";
 
 import styles from './TotalPoints.module.css';
 
 const TotalPoints: FC = () => {
-        const {totalPoints, multiplier} = useGameStore((state) => ({
-            totalPoints: state.totalPoints,
-            multiplier: state.multiplier
-        }));
+    const { totalPoints, multiplier } = useGameStore((state) => ({
+        totalPoints: state.totalPoints,
+        multiplier: state.multiplier,
+    }));
 
-        return <div className={styles.root}>
-            <DiamondPointsIcon className={styles.icon}/>
+    const [displayedPoints, setDisplayedPoints] = useState(totalPoints.toString());
+    const motionTotalPoints = useMotionValue(totalPoints);
+    const springTotalPoints = useSpring(motionTotalPoints, {
+        stiffness: 100,
+        damping: 20,
+    });
 
-            <span className={styles.value}>{totalPoints}</span>
+    useEffect(() => {
+        motionTotalPoints.set(totalPoints);
+    }, [totalPoints]);
 
-            <span className={styles.multiplier}><CrossIcon/> {multiplier}</span>
+    useEffect(() => {
+        return springTotalPoints.onChange((value) => {
+            setDisplayedPoints(Number(value).toFixed(1));
+        });
+    }, [springTotalPoints]);
+
+    return (
+        <div className={styles.root}>
+            <DiamondPointsIcon className={styles.icon} />
+
+            <motion.span className={styles.value}>
+                {displayedPoints}
+            </motion.span>
+
+            <span className={styles.multiplier}>
+                <CrossIcon /> {multiplier}
+            </span>
         </div>
-    }
-;
+    );
+};
 
 export default TotalPoints;
